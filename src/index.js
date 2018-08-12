67777d217c3f2e38688c4e6faa31c23ce708206b
wasifmiraz <wasifmiraz@mail.com>
2018-08-12 21:08:32 +0600

    server setup and connect with redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import path from 'path'
-const app =express()
+const app =express();
+
+
+app.post("/api/auth", (req, res) => {
+
+    res.status(400).json({error:{global:"invalid error credential"}})
+      });
 
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
   });
+
+
+
 
   app.listen(8080, () => console.log("Running on localhost:8080"));
