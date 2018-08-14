f8dd60aee773d492feb3b7f9bc224d2a15d7e4b3
wasifmiraz <wasifmiraz@mail.com>
2018-08-14 10:21:07 +0600

    eslint up set

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 const app =express();
 
+
 mongoose.connect('mongodb://localhost/users');
 
 const db = mongoose.connection;
@@ -12,30 +13,31 @@ db.once('open', () => {
   console.log('mongoosed database connected')
 });
 
-const Schema = mongoose.Schema;
-const userSchema = new Schema({
-  name:{
-    type:String,
-    required:true,
-    // index:true,
-    minlength:3
-  },
-  email: {
-    type:String,
-    required:true,
-    minlength:5,
-    lowercase:true
 
-  },
-  password:  {
-    type:String,
-    required:true,
-    minlength:3,
+// const Schema = mongoose.Schema;
+// const userSchema = new Schema({
+//   name:{
+//     type:String,
+//     required:true,
+//     // index:true,
+//     minlength:3
+//   },
+//   email: {
+//     type:String,
+//     required:true,
+//     minlength:5,
+//     lowercase:true
 
-  }
-});
+//   },
+//   password:  {
+//     type:String,
+//     required:true,
+//     minlength:3,
 
-const User = mongoose.model('User', userSchema);
+//   }
+// });
+
+// const User = mongoose.model('User', userSchema);
 
 app.post("/api/auth", (req, res) => {
 
@@ -46,7 +48,20 @@ app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
   });
 
-
-
-
   app.listen(8080, () => console.log("Running on localhost:8080"));
+
+//   app.get("/user", (req, res) => {
+//     const user = new User({
+//       name:"wasif miraz",
+//       email:"wasifmiraz@gmail.com",
+//       password:"4254253423"
+//     })
+//     user.save()
+//     .then(data => {
+// data.json({data})
+//     })
+//     .catch(err => console.log(err))
+//   });
+
+
+
