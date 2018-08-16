c320957fe0dc30ba130c2320a3f855a3d1a225cc
wasifmiraz <wasifmiraz@mail.com>
2018-08-16 15:22:13 +0600

    set users

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import path from 'path'
 import bodyParser from 'body-parser'
 import auth from './Routers/auth'
+import users from './Routers/users'
 import dotenv from 'dotenv'
 import Promise from 'bluebird'
 
@@ -17,6 +18,8 @@ const db = mongoose.connection;
 mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true});
 app.use(bodyParser.json());
 app.use("/api/auth", auth)
+
+app.use("/api/auth", users)
 
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
